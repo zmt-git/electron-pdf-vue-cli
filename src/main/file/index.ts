c9e5b92c107aadb4d322f32433f5df322f081522
ZMT <ZMT819@126.com>
2021-12-17 17:13:07 +0800

    feat js tip

--- src/main/file/index.ts
+++ src/main/file/index.ts
@@ -5,5 +5,6 @@ import { dir } from '../ipcMain'
 export function getPNGFlies () {
   const dirs = path.join(__dirname, dir.pending)
   let files = fs.readdirSync(dirs)
+  
   return files.filter(file => path.extname(file) === '.png')
 }
